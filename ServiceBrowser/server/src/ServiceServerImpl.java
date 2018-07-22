e179dc2e9f76b57e73bb2a6723ca3816c2879479
Dilik <1989Deny@gmail.com>
2018-07-21 21:53:47 -0400

    ServerBrowser

--- ServiceBrowser/server/src/ServiceServerImpl.java
+++ ServiceBrowser/server/src/ServiceServerImpl.java
@@ -2,43 +2,41 @@ import java.rmi.*;
 import java.util.*;
 import java.rmi.server.*;
 
-public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer{
 
-    HashMap serviceList;
+public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer  {
 
-    public ServiceServerImpl() throws RemoteException{
+    HashMap<String, Service> serviceList;
+
+
+    public ServiceServerImpl() throws RemoteException {
+	// start and set up services
 	setUpServices();
     }
 
-    //constructor 
-    private void setUpServices(){
-	serviceList = new HashMap();
+    private void setUpServices() {
+	serviceList = new HashMap<String, Service>();
 	serviceList.put("Dice Rolling Service", new DiceService());
 	serviceList.put("Day of the Week Service", new DayOfTheWeekService());
 	serviceList.put("Visual Music Service", new MiniMusicService());
     }
 
-    //get the list of service names. return is a list of objects
-    public Object[] getServiceList(){
+    public Object[] getServiceList() {
 	System.out.println("in remote");
 	return serviceList.keySet().toArray();
+
     }
 
-    //user selects the service and this method gets actual service based on
-    //the key corresponds to the service in the HashMap
-    public Service getService(Object serviceKey) throws RemoteException{
+    public Service getService(Object serviceKey) throws RemoteException {
 	Service theService = (Service) serviceList.get(serviceKey);
 	return theService;
     }
 
 
-    public static void main(String[] args){
-	try{
-	    Naming.rebind("ServiceServer", new ServiceServerImpl());
-	} catch (Exception ex){
-	    ex.printStackTrace();
-	}
+    public static void main (String[] args) {
 
+	try {
+	    Naming.rebind("ServiceServer", new ServiceServerImpl());
+	} catch(Exception ex) { }
 	System.out.println("Remote service is running");
     }
 
